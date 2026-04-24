658050ffec3cfeedf49a67975941a85a19741440
SAKTHIPRIYASATHISH <priyasakthi3837@gmail.com>
2026-04-24 22:30:19 +0530

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -4,14 +4,20 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
-const { protect } = require('./middleware/authMiddleware');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://financetracker-orcin.vercel.app'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
@@ -21,26 +27,6 @@ app.use('/api/transactions', transactionRoutes);
 // Health check
 app.get('/api/health', (req, res) => {
   res.json({ message: 'Server is running' });
-});
-
-// Temporary debug route
-app.get('/api/debug', protect, async (req, res) => {
-  const mongoose = require('mongoose');
-  const Transaction = require('./models/Transaction');
-
-  const userId = new mongoose.Types.ObjectId(req.user._id);
-
-  const all = await Transaction.find({});
-  const mine = await Transaction.find({ userId: req.user._id });
-  const agg = await Transaction.aggregate([{ $match: { userId: userId } }]);
-
-  res.json({
-    totalInDB: all.length,
-    myTransactions: mine.length,
-    aggMatch: agg.length,
-    myUserId: req.user._id.toString(),
-    firstTransaction: all[0] || null
-  });
 });
 
 const PORT = process.env.PORT || 5000;
